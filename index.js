629d09d7512b5f24882e5306b78458b0eab3535a
Scott Pribe <Scottpribe@gmail.com>
2021-03-06 21:17:33 -0500

    completed more problems and worked on regular expressions

--- index.js
+++ index.js
@@ -95,3 +95,40 @@ function getIndexToIns(arr, num) {
 
 getIndexToIns([40, 60], 50);
 //************************/
+
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+// The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+// Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+
+function mutation(arr) {
+	let first = arr[0].toLowerCase();
+	let second = arr[1].toLowerCase();
+
+	for (let i = 0; i < second.length; i++) {
+		if (first.indexOf(second[i]) < 0) return false;
+	}
+	return true;
+}
+
+mutation(["hello", "hey"]);
+
+//************************/
+
+//Chunky Monkey
+
+//Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+function chunkArrayInGroups(arr, size) {
+	let newArry = [];
+	for (let i = 0; i < arr.length; i += size) {
+		newArry.push(arr.slice(i, i + size));
+	}
+	return newArry;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+//************************/
